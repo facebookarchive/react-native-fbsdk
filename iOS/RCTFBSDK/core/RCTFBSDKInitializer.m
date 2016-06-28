8af3cfc0b6f03aea8f1f62984b55c882944cae74
Zhuowen Deng <dzhuowen@fb.com>
2016-06-27 17:54:20 -0700

    bump version 0.2.2

--- iOS/RCTFBSDK/core/RCTFBSDKInitializer.m
+++ iOS/RCTFBSDK/core/RCTFBSDKInitializer.m
@@ -40,7 +40,7 @@ RCT_EXPORT_MODULE();
 - (instancetype)init
 {
   if ((self = [super init])) {
-    [FBSDKSettings setUserAgentSuffix:@"ReactNative-v0.2.1"];
+    [FBSDKSettings setUserAgentSuffix:@"ReactNative-v0.2.2"];
   }
   return self;
 }
