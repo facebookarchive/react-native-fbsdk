75b1633cec0a50989a981fc3798053a8f52844cb
Zhuowen Deng <dzhuowen@fb.com>
2016-07-25 19:01:33 -0700

    Update sample app to rn-0.30

--- Sample/HelloFacebook/android/app/src/main/java/com/hellofacebook/MainActivity.java
+++ Sample/HelloFacebook/android/app/src/main/java/com/hellofacebook/MainActivity.java
@@ -1,23 +1,9 @@
 package com.hellofacebook;
 
-//Added import statement
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
 import com.facebook.react.ReactActivity;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-
-import java.util.Arrays;
-import java.util.List;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
-    //Added code
-    CallbackManager mCallbackManager;
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -27,33 +13,9 @@ public class MainActivity extends ReactActivity {
         return "HelloFacebook";
     }
 
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        //Added code
-        mCallbackManager = new CallbackManager.Factory().create();
-        ReactPackage packages[] = new ReactPackage[]{
-                new MainReactPackage(),
-                new FBSDKPackage(mCallbackManager),
-        };
-        return Arrays.<ReactPackage>asList(packages);
-    }
-
-    //Added code
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ Sample/HelloFacebook/android/app/src/main/java/com/hellofacebook/MainApplication.java
@@ -0,0 +1,55 @@
+package com.hellofacebook;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {    // Updated your class body:
+	private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+	protected static CallbackManager getCallbackManager() {
+		return mCallbackManager;
+	}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Initialize the SDK before executing any other operations.
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // Use AppEventsLogger to log custom events.
+        AppEventsLogger.activateApp(this);
+    }
+
+	private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+		@Override
+		protected boolean getUseDeveloperSupport() {
+		  return BuildConfig.DEBUG;
+		}
+
+		@Override
+		protected List<ReactPackage> getPackages() {
+		  return Arrays.<ReactPackage>asList(
+		      new MainReactPackage(),
+		      new FBSDKPackage(mCallbackManager)
+		  );
+		}
+	};
+
+	@Override
+	public ReactNativeHost getReactNativeHost() {
+	  return mReactNativeHost;
+	}
+
+}
