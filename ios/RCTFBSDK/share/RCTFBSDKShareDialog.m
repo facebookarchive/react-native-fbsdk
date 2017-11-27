c0ad26bab54d9ec3467a4c0703ec8538d347bf9d
Ralf Nieuwenhuizen <ralfnieuwenhuizen@gmail.com>
2017-11-27 15:20:51 +0100

    Allow setMode("native") on iOS

--- ios/RCTFBSDK/share/RCTFBSDKShareDialog.m
+++ ios/RCTFBSDK/share/RCTFBSDKShareDialog.m
@@ -28,6 +28,7 @@ RCT_ENUM_CONVERTER(FBSDKShareDialogMode, (@{
   @"automatic": @(FBSDKShareDialogModeAutomatic),
   @"browser": @(FBSDKShareDialogModeBrowser),
   @"webview": @(FBSDKShareDialogModeWeb),
+  @"native": @(FBSDKShareDialogModeNative),
 }), FBSDKShareDialogModeAutomatic, unsignedLongValue)
 
 @end
