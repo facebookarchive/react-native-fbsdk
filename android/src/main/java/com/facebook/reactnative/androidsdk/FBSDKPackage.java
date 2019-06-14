6fa1529ba0c6a2e0d6264f6040271855547ff31f
Janic Duplessis <janicduplessis@gmail.com>
2019-06-14 16:11:59 -0400

    Remove version from FBSDKPackage

--- android/src/main/java/com/facebook/reactnative/androidsdk/FBSDKPackage.java
+++ android/src/main/java/com/facebook/reactnative/androidsdk/FBSDKPackage.java
@@ -33,9 +33,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class FBSDKPackage implements ReactPackage {
-
-    public static final String VERSION_TO_RELEASE = "ReactNative-v0.9.0";
-
     private CallbackManager mCallbackManager;
     public FBSDKPackage(CallbackManager callbackManager) {
         mCallbackManager = callbackManager;
